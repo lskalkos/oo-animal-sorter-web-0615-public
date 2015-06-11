8221a4a51a2460d191435a7f7647c19bf38bf0bf
Lia Skalkos <hello@liaskalkos.net>
2015-06-11 08:29:30 -0400

    Complete class.

--- lib/animal_sorter.rb
+++ lib/animal_sorter.rb
@@ -1,7 +1,36 @@
 # Build a class AnimalSorter
 
+class AnimalSorter
+
 # Make sure there are two constants that AnimalSorter knows about, 
 # SEA_ANIMALS and LAND_ANIMALS
+  SEA_ANIMALS = ["marlin", "octopus", "fish"]
+  LAND_ANIMALS = ["aardvark", "cat", "elephant"]
+
+  def initialize(array)
+    @array = array
+  end
+
+  def to_a
+    #takes ["marlin", "aardvark", "octopus", "cat", "fish", "elephant"]
+    #return value should have .length == 2
+    sea_animals = []
+    land_animals = []
+
+    @array.each do |animal|
+      if SEA_ANIMALS.include?(animal)
+        sea_animals << animal
+      else 
+        land_animals << animal
+      end
+    end
+
+    [sea_animals, land_animals]
+          
+  end
+
+end
+
 
 # AnimalSorter should be able to accept an array of animals on 
 # initialization.
